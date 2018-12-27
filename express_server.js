a5b16b9ef16d12ebcad8a1bd3c4bbf92e0e2f7e7
Oleksandr09 <okatiukha@gmail.com>
2018-12-27 18:59:44 +0000

    modified index and show pages with forms for deleting and updating urls

--- express_server.js
+++ express_server.js
@@ -1,15 +1,25 @@
-var express = require("express");
-var app = express();
-var PORT = 8080;
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const PORT = 8080;
 
 app.set("view engine", "ejs");
-
+app.use(bodyParser.urlencoded({extended: true}));
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRandomString() {
+	var text = "";
+  	var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  	for (var i = 0; i < 6; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+    return text;
+}
+
+// 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -23,12 +33,44 @@ app.get("/urls", (req, res) => {
     res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+  });
+
 app.get("/urls/:id", (req, res) => {
     let templateVars = { shortURL: req.params.id,
                          longURL: urlDatabase[req.params.id] 
                        };
     res.render("urls_show", templateVars);
 });
+
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL];
+  console.log(longURL);
+  res.redirect(longURL);
+});
+
+app.post("/urls", (req, res) => {
+    let shortURL = generateRandomString();
+    let longURL = req.body.longURL;
+    urlDatabase[shortURL] = longURL;
+    console.log(urlDatabase);
+    res.redirect("/urls/" + shortURL);
+
+})
+
+app.post("/urls/:id", (req, res) => {
+  let longURL = req.body.longURLUpdate;
+  urlDatabase[req.params.id] = "http://" + longURL;
+  console.log(urlDatabase);
+  res.redirect("/urls");
+})
+
+app.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id];
+  console.log(urlDatabase);
+  res.redirect("/urls");
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
